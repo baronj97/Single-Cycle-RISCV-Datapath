5fe8a68a101276b8bab938f6610459ef693e9833
Jacob Baron <jab644@drexel.edu>
2019-02-11 13:58:50 -0500

    Created an assembly to machine code translator

--- main.c
+++ main.c
@@ -7,16 +7,22 @@
 #include "memory.h"
 #include "writeback.h"
 #include "execute.h"
+#include "assembler.h"
+#include "config.h"
+
 int main(int argc, char** argv){
-	// Declare some file pointers
+	
+    // Declare some file pointers
 	FILE *ifp, *dfp, *rfp;
 	// Open the instruction memory file
 	ifp = fopen("instruction_memory.txt", "r");
 
 	// Load the instruction memory into a structure
 	struct instruction_memory instructions;
-       	init_instruction_memory(ifp, &instructions);
-	print_instructions(&instructions);
+    //init_instruction_memory(ifp, &instructions);
+	assembly_to_machine(ifp, &instructions);
+
+    print_instructions(&instructions);
 	// Close the file
 	fclose(ifp);
 
@@ -55,18 +61,9 @@ int main(int argc, char** argv){
 	// This is all testing... will need to move into the for-loop
 	unsigned int test = 0b00000000000100000000000110000000;
 	// Declare a decode struct and decode the instruction
-	/*struct decode_info decoded_instruction;
-	decode_instruction(test, &decoded_instruction, &registers);
-	
-	printf("Valid: %i\n",decoded_instruction.r_type.valid);
-	printf("Opcode: %i\n", decoded_instruction.r_type.opcode);
-	printf("Dest Reg: %i\n", decoded_instruction.r_type.dest_reg);
-	printf("Source reg1: %i\n", decoded_instruction.r_type.source_reg_1);
-	printf("Source reg1 value: %i\n", decoded_instruction.r_type.source_reg_1_value);
-	*/
 	dump_registers_to_data_memory(&registers, dfp);
-	
 	fclose(dfp);
+
 
 	return 0;
 }
